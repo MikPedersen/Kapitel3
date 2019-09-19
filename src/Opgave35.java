45f00216382b2c5d77093200c3f78e835ac842c5
Mik Pedersen <45688364+MikPedersen@users.noreply.github.com>
2019-09-19 14:13:14 +0200

    Sidste opgave tilføjet

--- src/Opgave35.java
+++ src/Opgave35.java
@@ -1,3 +1,14 @@
+import java.util.Scanner;
+
 public class Opgave35 {
-    
+    public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+        System.out.println("Please enter a number");
+        int inputNumber = input.nextInt();
+        if (inputNumber % 2 == 0)
+            System.out.println(inputNumber + " is an even number");
+        else
+            System.out.println(inputNumber+ " is an odd number");
+
+    }
 }
